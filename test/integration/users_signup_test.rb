71a254c85794437528b4e3f45dcf18a8336be6a6
Evan <srichsun@gmail.com>
2017-10-25 15:38:10 +0800

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: "",
+                                        email: "user@invalid",
+                                        password: "foo",
+                                        password_confirmation: "foobar" }}
+    end
+
+    assert_template 'users/new'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference "User.count", 1 do
+      post users_path, params: { user: { name: "user_wu",
+                                        email: "user_wu@gmail.com",
+                                        password: "fooooo",
+                                        password_confirmation: "fooooo"}}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+
+end
